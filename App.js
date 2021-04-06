767218ae7696cc1acf0079c16fa029dd772b84e1
FlashGod123 <67604319+FlashGod123@users.noreply.github.com>
2021-04-05 17:09:33 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import SignupLoginScreen from './screens/SignupLoginScreen';
+
+export default class App extends React.Component {
+  render(){
+    return(
+      <View>
+        <SignupLoginScreen/>
+      </View>
+    )
+  }
+}
+
+
